c94cb9107089989342f7032784c6e87e85e1c7d5
Eduardo Almeida <eduardoalme23@gmail.com>
2025-08-27 21:04:29 -0300

    Codigos.JavaScript

---
+++ Instruçãoerepetição.js
@@ -0,0 +1,14 @@
+// Variavel que representa a previsção do Tempo
+// Pode ser "rainy" (chuvoso), "sunny" (ensolarado) ou "cloudy" (nublado)
+
+let watherforecat = "rainy"; 
+
+// Analogia com a vida real: Deccidindo se devemos levar um guarda - chuva
+// Vamos adicionar uma condição extra para cobrir outro cenário 
+
+if (weatherforest === "rainy") {
+    console.log("devemos levar um gurada chuva");
+}
+else { 
+    console.log("Não devemos levar um guarda-chuva");
+}
